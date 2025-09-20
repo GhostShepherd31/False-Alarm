f907baf9fb80c23137e61e6b10fd5e0ae3be317a
GhostShepherd31 <91279144+GhostShepherd31@users.noreply.github.com>
2025-09-20 03:13:43 -0400

    Create state.js

---
+++ state.js
@@ -0,0 +1,14 @@
+const STANDARD = 5;
+
+const state = {
+  profile: {
+    userGender: '',
+    partnerGender: '',
+    notes: ''
+  },
+  qualities: []
+};
+
+function byId(id) {
+  return document.getElementById(id);
+}
